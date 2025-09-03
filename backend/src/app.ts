a6969631324316a384a9c0cbce9d6f97932322af
Aryan Singh <aryansingh@MacBookAir.mshome.net>
2025-09-03 16:20:15 +0530

    Production fix#2

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -30,7 +30,7 @@ app.use("/api/status", (req, res) => {
 })
 
 if(process.env.NODE_ENV !== "production"){
-    const PORT = process.env.PORT || 5000;
+    const PORT = 5000;
     app.listen(PORT, () => {
         console.log(`Server is running on port ${PORT}`);
     });
