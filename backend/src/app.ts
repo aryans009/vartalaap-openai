fa5272fa7d1e316f61ba0c43a6aa0f5577afe7b8
Aryan Singh <aryansingh@Aryans-MacBook-Air.local>
2025-09-03 15:48:06 +0530

    Production error fix

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -26,4 +26,11 @@ app.use(cookieParser(process.env.COOKIE_SECRET));
 // app.use(morgan("dev"));
 app.use("/api/v1",appRoutes);
 
+if(process.env.NODE_ENV !== "production"){
+    const PORT = process.env.PORT || 5000;
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+    });
+}
+
 export default app;
